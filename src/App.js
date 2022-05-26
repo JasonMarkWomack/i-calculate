473141ec0677b733260d8bdecac105b3e765837c
Jason Womack <jasonwomack@Jasons-MacBook-Air.local>
2022-05-26 15:53:06 -0500

    rset up ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
   return(
-      <div className="App">          
+      <div className="App">  
+      <h1>I - Calculate</h1>    
+          
+     
+     <form>
+                <input type="text" id="result" readOnly />   
+                <input type="text" id="num" placeholder="enter a number" />
+               <button>Add</button>
+               <button>Clear</button>
+        </form>
      </div>
   )
 }
